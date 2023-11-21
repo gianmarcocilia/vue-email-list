619a9921ad849c30c11f970ec1e80a6b05cbb444
gianmarcocilia <gianmarcocilia97@gmail.com>
2023-11-21 13:36:15 +0100

    add: request bonus

--- js/script.js
+++ js/script.js
@@ -7,6 +7,7 @@ const app = createApp({
     },
     methods: {
         generateEmail() {
+            this.emails = [];
             for(let i = 0; i < 10; i++) {
                 axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then((resp) => {
                     console.log(resp);

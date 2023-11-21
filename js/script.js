d8879d60d992801f626b2b5c4d8762fbdc701031
gianmarcocilia <gianmarcocilia97@gmail.com>
2023-11-21 13:29:55 +0100

    add: generate 10 emails

---
+++ js/script.js
@@ -0,0 +1,19 @@
+const {createApp} = Vue;
+const app = createApp({
+    data() {
+        return {
+            emails: []
+        }
+    },
+    methods: {
+        generateEmail() {
+            for(let i = 0; i < 10; i++) {
+                axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then((resp) => {
+                    console.log(resp);
+                    this.emails.push(resp.data.response);
+                })
+            }
+            console.log(this.emails);
+        }
+    }
+}).mount("#app");
